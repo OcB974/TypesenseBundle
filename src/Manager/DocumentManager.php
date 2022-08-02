d8f4f52ba62cd2e034efe943817e828afa1f48c2
Thomas <techer_thomas@yahoo.fr>
2022-08-02 13:54:14 +0400

    Fix bug
    Bug : When truncate table, return error Document with Id exist. Before index, retrieve the document with same id and update

--- src/Manager/DocumentManager.php
+++ src/Manager/DocumentManager.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace ACSEO\TypesenseBundle\Manager;
 
 use ACSEO\TypesenseBundle\Client\TypesenseClient;
+use Exception;
 
 class DocumentManager
 {
@@ -13,6 +14,15 @@ class DocumentManager
     public function __construct(TypesenseClient $client)
     {
         $this->client = $client;
+    }
+
+    public function retrieveDocument(string $collection, $id)
+    {
+        try {
+            return $this->client->collections[$collection]->documents[$id]->retrieve();
+        } catch( Exception $e ) {
+        }
+        return null ;
     }
 
     public function delete($collection, $id)
@@ -29,8 +39,10 @@ class DocumentManager
         if (!$this->client->isOperationnal()) {
             return null;
         }
-
-        return $this->client->collections[$collection]->documents->create($data);
+        $retrieveDocument = $this->retrieveDocument($collection, $data['id']);
+        if(empty($retrieveDocument)): // Not found document, create
+            return $this->client->collections[$collection]->documents->create($data);
+        endif;
     }
 
     public function import(string $collection, array $data, string $action = 'create'): array
